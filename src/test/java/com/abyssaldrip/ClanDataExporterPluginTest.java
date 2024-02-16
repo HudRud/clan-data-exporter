acddb5753108dcfa908e15d468d851f8193e24fd
HudRud <64253695+HudRud@users.noreply.github.com>
2024-02-16 10:35:33 +0200

    Created base for plugin, added icon.png

--- src/test/java/com/abyssaldrip/ClanDataExporterPluginTest.java
+++ src/test/java/com/abyssaldrip/ClanDataExporterPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.abyssaldrip;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class ClanDataExporterPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(ClanDataExporterPlugin.class);
 		RuneLite.main(args);
 	}
 }
